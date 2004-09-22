cc9f3bd94613e8684e4fa824eaa514bff6d16102
Benjamin Reitzammer <cpan@nur-eine-i.de>
2004-09-22 09:02:37 +0000

    Import of BENJAMIN/Kwiki-ReferrerLog-0.05 from CPAN.
    gitpan-cpan-distribution: Kwiki-ReferrerLog
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         BENJAMIN/Kwiki-ReferrerLog-0.05.tar.gz
    gitpan-cpan-author:       BENJAMIN
    gitpan-cpan-maturity:     released

--- lib/Kwiki/ReferrerLog.pm
+++ lib/Kwiki/ReferrerLog.pm
@@ -1,44 +1,66 @@
 package Kwiki::ReferrerLog;
 use strict;
 use warnings;
-use Kwiki::Display '-Base';
+use Kwiki::Plugin '-Base';
 use mixin 'Kwiki::Installer';
 use Storable qw(lock_store lock_retrieve);
 use POSIX qw(strftime);
-our $VERSION = '0.01';
+our $VERSION = '0.05';
 
 our $DAY_SECONDS = 86400;
 
-const config_file => 'referrerlog.yaml';
 const class_id    => 'referrerlog';
 const class_title => 'ReferrerLog Display';
-const css_file    => 'referrerlog.css';
+const config_file => 'referrerlog.yaml';
+
+const css_file             => 'referrerlog.css';
+const referrerlog_template => 'referrerlog_content.html';
+
 const log_file    => 'referrers.log';
 
-
+sub init {
+    super;
+    $self->hub->add_post_process($self->class_id, 'log_referrer');
+}
 
 sub register {
-    $self->SUPER::register(@_);
-
     my $registry = shift;
-    $registry->add(action => 'referrerlog');
+    $registry->add(preload => 'referrerlog');
+
+    $registry->add(action => 'show_referrerlog');
     $registry->add(toolbar => 'referrerlog_button',
-                   template => 'referrerlog_button.html',
-                  );
+                   template => 'referrerlog_button.html');
 }
 
-sub display {
-    $self->log_referrer;
-    $self->SUPER::display(@_);
+sub log_referrer {
+    my $ref = $ENV{HTTP_REFERER};
+    return unless $ref;
+
+    my $excl_subd = $self->config->exclude_subdomains;
+    my @test = ();
+    grep {
+        if ( $excl_subd ) { return if $ref =~ m!^http://[a-zA-Z]*\.*$_!; }
+        else { return if $ref =~ m!^http://$_! ; }
+    } @{$self->config->exclude_referrers} if ref $self->config->exclude_referrers;
+
+    my $log = $self->load_log;
+    $log->{$ref}->[0]++ ;      # referrer count
+    $log->{$ref}->[1] = time;  # time
+    $log->{$ref}->[2] = $self->pages->current->uri;  # where did it go?
+
+    $self->delete_old_logs($log);
+    $self->store_log($log);
 }
 
 
-sub referrerlog {
+sub show_referrerlog {
+    my $reflength = $self->config->truncate_referrers;
+
     my $log = $self->load_log;
     my $refs = [];
     foreach (keys %$log) {
       push @$refs, {'visitcount' => $log->{$_}->[0], 'time'   => $log->{$_}->[1],
-                    'uri'        => $log->{$_}->[2], 'refuri' => $_ };
+                    'uri'    => $log->{$_}->[2],     'refuri' => $_};
     }
 
     # puts the latest referrers on the top of the list
@@ -48,25 +70,6 @@ sub referrerlog {
         screen_title => $self->class_title,
         'log' => $refs,
     );
-}
-
-
-sub log_referrer {
-    my $ref = $ENV{HTTP_REFERER};
-    my $base_uri = eval {$self->hub->config->site_uri};
-    die "The configuration parameter 'site_uri' has to be specified".
-        "in the config file! \n $@"
-      if $@;
-
-    if ( $ref && $ref !~ /^$base_uri/ ) {
-        my $log = $self->load_log;
-        $log->{$ref}->[0]++ ;      # referrer count
-        $log->{$ref}->[1] = time;  # time
-        $log->{$ref}->[2] = $self->pages->current->uri;  # where did it go?
-
-        $self->delete_old_logs($log);
-        $self->store_log($log);
-    }
 }
 
 
@@ -118,51 +121,52 @@ appropriate methods (see below).
 
 =head1 OPTIONS
 
-=head3 Mandatory
-
-Additionally to the standard installation procedure of a Kwiki::Plugin this
-module needs two more options set for functioning correctly.
+The following configurationoptions can be overriden in your config.yaml file:
 
 =over 4
 
-=item
+=item C<keep_days>
 
-L<site_uri>: This module needs the site_uri parameter configured in your
-config.yaml (or what else your config file might be) and set to a correct value.
-The value of  the site_uri parameter will be used for sorting out the
-internal referrers. So setting this correctly might be of interest to you.
-
-Example: If your Kwiki installation is located at
-http://www.example.com/kwiki/index.cgi you should set the site_uri option to
-http://www.example.com/kwiki/
-
-=item
-
-L<display_class>: You have to configure the Kwiki::ReferrerLog module as your
-plugin for the display-action. This sounds harder than it is.
-
-Simply set add the following line to you config.yaml:
-display_class: Kwiki::ReferrerLog
-
-=back
-
-=head3 Optional
-
-The following options can be overriden in your config.yaml file:
-
-=over 4
-
-=item
-
-L<keep_days>: This determines after which number of days, a log entry gets
+ This determines after which number of days, a log entry gets
 deleted from the store, if there weren't any requests coming in from that
 URL. Don't set this value too high. (default is: 2)
 
-=item
+=item C<date_format>
 
-L<date_format>: The date format that is used in the display of logged referrers.
+ The date format that is used in the display of logged referrers.
 This is directly passed to the L<strftime> function of the L<POSIX> package,
 so don't hate me, if you specify a wrong pattern (default is: %d.%m.%Y %H:%M)
+
+=item C<exclude_referrers>
+
+ This is a list of domains, that won't be logged as
+referrers. You should set this at least to the domain your Kwiki installation is
+running on, so the clicking around on your wiki won't result in a flood of
+referrers showing up in you referrer log.
+The list must be in a YAML anonymous array format, that looks like this:
+
+=begin text
+
+exclude_referrers:
+- example.com
+- site.org
+
+=end text
+
+=item C<exclude_subdomains>
+
+This boolean value specifies (0 is false, everything else
+is true), is subdomains of the configured L<exclude_referrers> domains
+should be exluded too, or if they should be logged.
+Example: If L<exclude_referrers> is set to exclude the domain example.com, then
+requests coming from sample.example.com will be logged if L<exclude_subdomains>
+is set to 0, while they won't be logged when L<exclude_subdomains> is set to 1.
+(default value: 1)
+
+=item C<truncate_referrers>
+
+ This number defines, after how much characters the referring
+address should be truncated in the ReferrerLog Display (default value: 40).
 
 =back
 
@@ -175,29 +179,29 @@ The module provides the following methods, which can be overridden:
 
 =over 4
 
-=item
+=item C<show_referrerlog()>
 
-L<referrerlog()>: This method loads the stored referrers and renders the
+This method loads the stored referrers and renders the
 template for showing the result back to the browser. It is registered as the
 action-method for this module.
 
-=item
+=item C<log_referrer>
 
-L<log_referrer>: This method logs the referrer. For this it checks if the
+This method logs the referrer. For this it checks if the
 referrer comes from an external site. If this is not the case, the control
 flow leaves the method. Otherwise the stored referrers are loaded and the
 current one is appended to the list. Afterwards it checks, if some
 of the referrers are older than the configured value for L<keep_days>. If this
 is the case, the corresponding referrers are deleted from the list.
 
-=item
+=item C<file_path>
 
-L<file_path>: This method returns a relative path to the location of the
-referrer log file (default: L<plugin_directory/referrerlog/referrers.log>).
+This method returns a relative path to the location of the
+referrer log file (default: F<plugin_directory/referrerlog/referrers.log>).
 
-=item
+=item C<load_log>
 
-L<load_log>: This method loads the stored referrer entries and returns them as
+This method loads the stored referrer entries and returns them as
 an hash reference, that contains, keyed by referring URLs, array references.
 
 The following example should clarify the structure:
@@ -211,21 +215,21 @@ $hashref = { 'http://www.example.com/ref1' =>
 
 =end text
 
-=item
+=item C<store_log>
 
-L<store_log>: This method takes a hash reference as described above and stores
+This method takes a hash reference as described above and stores
 it, so that the L<load_log> method can retrieve it later.
 
 
-=item
+=item C<date_fmt>
 
-L<date_fmt>: This method uses the L<date_format> configuration option, to format
+This method uses the L<date_format> configuration option, to format
 the timestamp that is passed as the first and only parameter, as the user/admin
 wishes.
 
-=item
+=item C<delete_old_logs>
 
-L<delete_old_logs>: This method is called before every call to L<store_log>, and
+This method is called before every call to L<store_log>, and
 deletes entries, that are older than the number of days specified by the
 keep_days option (see above).
 
@@ -253,39 +257,43 @@ storage
 __config/referrerlog.yaml__
 keep_days: 2
 date_format: %d.%m.%Y %H:%M
+exclude_referrers:
+exclude_subdomains: 1
+truncate_referrers: 40
 __template/tt2/referrerlog_button.html__
-<a href="[% script_name %]?action=referrerlog" title="Log of Referrers">
+<a href="[% script_name %]?action=show_referrerlog" title="Log of Referrers">
 [% INCLUDE referrerlog_button_icon.html %]
 </a>
 __template/tt2/referrerlog_button_icon.html__
 Referrers
 __template/tt2/referrerlog_content.html__
-[% IF hub.action == 'referrerlog' %]
-  <table id="reflog_table">
-    <tr>
-      <th class="reflog_head">Viewed Page</th>
-      <th class="reflog_head">Referring URL</th>
-      <th class="reflog_head">Last Request</th>
-      <th class="reflog_head">Count</th>
-    </tr>
-  [% FOR  ref = log -%]
-    <tr>
-      <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">[% ref.uri %]</td>
-      <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">
-        <a href="[% ref.refuri %]" title="external link to [% ref.refuri %]">[% ref.refuri %]</a>
-        <img src="/images/foreignlinkglyph.png" alt="" border="0" />
-      </td>
-      <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">[% self.date_fmt(ref.time) %]</td>
-      <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">[% ref.visitcount %]</td>
-    </tr>
-  [% END %]
-  </table>
-[% ELSE %]
-  <div class="wiki">
-  [% page_html -%]
-  </div>
-  [% INCLUDE display_changed_by.html %]
+This is a list of sites, that refer to this website. The 'count' column denotes
+the number of visitors, that have arrived from the corresponding site until now.
+<table id="reflog_table">
+  <tr>
+    <th class="reflog_head">Viewed Page</th>
+    <th class="reflog_head">Referring URL</th>
+    <th class="reflog_head">Last Request</th>
+    <th class="reflog_head">Count</th>
+  </tr>
+[% FOR  ref = log -%]
+  <tr>
+    <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">
+      <a href="[% script_name %]?[% ref.uri %]" title="link to [% ref.refuri %]">[% ref.uri %]</a></td>
+    <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">
+      <a href="[% ref.refuri %]" title="external link to [% ref.refuri %]">
+      [% IF ref.refuri.length > self.config.truncate_referrers %]
+        [% FILTER truncate(self.config.truncate_referrers) %] [% ref.refuri %] [% END %]
+      [% ELSE %]
+        [% ref.refuri %]
+      [% END %]
+      </a>
+    </td>
+    <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">[% self.date_fmt(ref.time) %]</td>
+    <td class="[% 'odd_' IF loop.count % 2 == 0 %]reflog_line">[% ref.visitcount %]</td>
+  </tr>
 [% END %]
+</table>
 __css/referrerlog.css__
 #reflog_table { width:100%; }
 .reflog_head  {}
